3b9ea90a8c1848d30da3b55aa21444766b4c79a6
valentinojehaut <jehaut23@gmail.com>
2021-08-22 12:02:38 +0800

    Update #3 B
    - Make new JS files and move some scripts to their respected files just to declutter the main index.html
    - Added couple of pictures to put it on the "My Work/Portfolio" section
    - Revamp the "My Work/Portfolio" section to look a bit more sleek and modern

--- assets/navbar.js
+++ assets/navbar.js
@@ -1,7 +1,21 @@
+// Activate Offcanvas Bootstrap On Navbar
 (function () {
-    'use strict'
-  
-    document.querySelector('[data-bs-toggle="offcanvas"]').addEventListener('click', function () {
-      document.querySelector('.offcanvas-collapse').classList.toggle('open')
-    })
+  'use strict'
+
+  document.querySelector('[data-bs-toggle="offcanvas"]').addEventListener('click', function () {
+    document.querySelector('.offcanvas-collapse').classList.toggle('open')
+  })
   })()
+
+// Activate Shrink Navbar
+var nav = document.querySelector('nav');
+
+window.addEventListener('scroll', function () {
+  if (window.pageYOffset > 500) {
+    nav.classList.add('bg-dark', 'shadow');
+    nav.classList.add('shrink');
+  } else {
+    nav.classList.remove('bg-dark', 'shadow');
+    nav.classList.remove('shrink');
+  }
+});
